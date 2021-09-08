be8d3c6642b58feae31e8b5932359aade5146c5f
brittney rawlings <bn.rawlings@gmail.com>
2021-09-08 14:38:26 -0500

    exer 1 complete

--- exercise_1/index.js
+++ exercise_1/index.js
@@ -1,33 +1,33 @@
-const PORT = 3000;
+const PORT = 3004;
 const express = require("express");
 const app = express();
 
 // app.get("/helloThere", (req, res) => {
 //     res.send("Why hello there!");
 // });
-// app.listen(`Listening on ${PORT}`)
+// app.listen(PORT,console.log(`Listening on ${PORT}`))
 
-app.post("/postEmoji", (req, res) => {
-    res.send("🥭 is 🔥")
-})
-app.listen(`Listening on ${PORT}`)
+// app.post("/postEmoji", (req, res) => {
+//     res.send("🥭 is 🔥")
+// })
+// app.listen(PORT,console.log(`Listening on ${PORT}`))
 
 // app.post("/wrongRoute", (req, res) => {
 //     res.send("I wonder what's wrong with this route 🤔?")
 // })
-// app.listen(`Listening on ${PORT}`)
+// app.listen(PORT,console.log(`Listening on ${PORT}`))
 
 // app.get("/dogFood", (req,res) => {
 //     res.send("Happy 🐶 loves yummy dog food!")
 // })
-// app.listen(`Listening on ${PORT}`)
+// app.listen(PORT,console.log(`Listening on ${PORT}`))
 
 // app.get("/fruitsmoothie",(req, res) => {
 //     res.send("A 🥭, 🍍, 🍓 make a wonderful smoothie!")
 // })
-// app.listen(`Listening on ${PORT}`)
+// app.listen(PORT,console.log(`Listening on ${PORT}`))
 
-// app.post("/lastOne", (req, res) => {
-//     res.send("I think you've got it!")
-// })
-// app.listen(`Listening on port ${PORT}`)
+app.post("/lastOne", (req, res) => {
+    res.send("I think you've got it!")
+})
+app.listen(PORT,console.log(`Listening on ${PORT}`))
